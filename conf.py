c6d3788b06ca81aae6233019e90393e0d110de23
Mikael Hernrup <mikael@hernrup.se>
2015-09-13 16:10:50 +0200

    initial commit

---
+++ conf.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+import os
+import sys
+
+
+sys.path.append(os.curdir)
+
+SITEURL = ''
+RELATIVE_URLS = False
+
+FEED_ALL_ATOM = 'feeds/all.atom.xml'
+CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
+
+DELETE_OUTPUT_DIRECTORY = True
+OUTPUT_PATH = '/output/'
+
+# Following items are often useful when publishing
+
+#DISQUS_SITENAME = ""
+#GOOGLE_ANALYTICS = ""
